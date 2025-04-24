f47367a6615f8531dac3d290297d997e0c6ba6c3
clarkieyow <clarkposadas2@gmail.com>
2025-04-24 13:07:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Website loaded successfully!");
 });
+
+// JavaScript for burger menu toggle
+document.getElementById('burger-menu').addEventListener('click', function() {
+    var nav = document.querySelector('header nav');
+    var burger = document.getElementById('burger-menu');
+    
+    nav.classList.toggle('active');  // Toggle the "active" class to show/hide the menu
+    burger.classList.toggle('active');  // Toggle the "active" class to animate the burger menu
+});
